162855cea14e5a551abf5598f36f74a73822ef1e
Jonathan Grim <jonjongrim@gmail.com>
2017-02-25 18:06:17 -0500

    Rename to better describe tool

--- last_modified.py
+++ last_modified.py
@@ -6,13 +6,13 @@ import datetime
 
 
 def list_files(dir):
-    file_atime_list = []
+    file_mtime_list = []
     for top, sub_dir, file_list in os.walk(dir):
         if file_list:
             for file in file_list:
-                file_atime_list.append((file, os.path.getmtime(os.path.join(top, file))))
+                file_mtime_list.append((file, os.path.getmtime(os.path.join(top, file))))
 
-    sorted_file_list = sorted(file_atime_list, key=lambda x: x[1], reverse=True)
+    sorted_file_list = sorted(file_mtime_list, key=lambda x: x[1], reverse=True)
     for file in sorted_file_list:
         print(file[0], ": ", datetime.datetime.fromtimestamp(file[1]).strftime('%c'))
 
