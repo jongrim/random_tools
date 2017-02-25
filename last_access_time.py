3b67b52a1bfc826a14f7f710050ac95b06281524
Jonathan Grim <jonjongrim@gmail.com>
2017-02-25 17:11:31 -0500

    First commit

---
+++ last_access_time.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import os
+import sys
+import datetime
+
+
+def list_files(dir):
+    file_atime_list = []
+    for top, sub_dir, file_list in os.walk(dir):
+        if file_list:
+            for file in file_list:
+                file_atime_list.append((file, os.path.getmtime(os.path.join(top, file))))
+
+    sorted_file_list = sorted(file_atime_list, key=lambda x: x[1], reverse=True)
+    for file in sorted_file_list:
+        print(file[0], ": ", datetime.datetime.fromtimestamp(file[1]).strftime('%c'))
+
+
+if __name__ == '__main__':
+    if os.path.isdir(sys.argv[1]):
+        list_files(sys.argv[1])
